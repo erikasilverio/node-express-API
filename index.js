7b7632579ab5d1ea7a720839876e21a21ba9a83e
Erika Silverio <cms.silverio@gmail.com>
2024-06-15 03:12:00 -0300

    Rutas // Node.js

--- index.js
+++ index.js
@@ -3,9 +3,42 @@ const app = express();
 
 // http://localhost:3000/
 
+// app.use(express.static("public"));
+
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
 app.get('/', (req, res) => {
     res.send("Hola Express!!!");
 });
+
+
+app.get('/factura', (req, res) => {
+    // LOGIN
+    res.sendFile(path.join(__dirname, 'private', 'factura.html'));
+});
+
+
+// app.get("/frutas", (req, res) => {
+//     res.sendFile(path.join(__dirname, "frutas.json"));
+// });
+
+app.get("/frutas", (req, res) => {
+    console.log(req.query);
+    res.sendFile(path.join(__dirname, "frutas.json"));
+});
+
+
+
+app.get('/frutas/:id', (req, res) => {
+    console.log(req.params.id);
+    res.send('Una Fruta con el id: ' + req.params.id);
+});
+
+
 
 const PORT = 3000
 
