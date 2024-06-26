27bb14a45db57a28247a0b975b761d4a2ca7ac1b
Erika Silverio <cms.silverio@gmail.com>
2024-06-26 00:34:46 -0300

    Node 05 - MySQL

--- routes/productos.router.js
+++ routes/productos.router.js
@@ -2,79 +2,87 @@ const express = require("express");
 const router = express.Router();
 
 
-const productos = [
-    {id: 1, nombre: "Producto Nro. 1" },
-    {id: 3, nombre: "Producto Nro. 3" },
-    {id: 4, nombre: "Producto Nro. 4" },
-    {id: 7, nombre: "Producto Nro. 7" },
-]
-
-router.get("/", (req, res) => {
-    res.json(productos);
-});
 
 
-// /productos/:id
-router.get("/:id", (req, res) => {
-    const { id } = req.params; 
+// const productos = [
+//     {id: 1, nombre: "Producto Nro. 1" },
+//     {id: 3, nombre: "Producto Nro. 3" },
+//     {id: 4, nombre: "Producto Nro. 4" },
+//     {id: 7, nombre: "Producto Nro. 7" },
+// ]
+
+const controller = require("../controllers/productos.controller");
+
+router.get("/", controller.index);
 
 
-   
+// // /productos/:id
+// router.get("/:id", (req, res) => {
+//     const { id } = req.params; 
+
+//     const producto = productos.find((elemento) => elemento.id == id);
+//     // console.log(producto);
+
+//     res.json(producto);
+// });
 
 
-    res.json(producto);
-});
 
-
-
-router.post("/", (req, res) => {
+// router.post("/", (req, res) => {
     // console.log(req.body, productos[productos.length - 1], productos.length);
 
-    const id = productos[productos.length - 1].id + 1;
+//     const id = productos[productos.length - 1].id + 1;
 
-    const producto = {
-        id: id,
-        nombre: req.body.nombre,
-    };
+//     const producto = {
+//         id: id,
+//         nombre: req.body.nombre,
+//     };
 
-    productos.push(producto);
+//     productos.push(producto);
 
-    res.status(201).send(producto);
-});
+//     res.status(201).send(producto);
+// });
+
+
+router.get("/:id", controller.show);
+router.post("/", controller.store);
 
 // /productos/:id
 
-router.put("/:id",(req,res) => {
-    const{ id } = req.params;
-    const { nombre } = req.body;
+// router.put("/:id",(req,res) => {
+//     const{ id } = req.params;
+//     const { nombre } = req.body;
 
-    const producto = productos.find((elemento) => elemento.id == id);
-    // console.log(producto);
-    if (!producto){
-        return res.status(404).json({ error: "No se encontro el producto"});
-    }
+//     const producto = productos.find((elemento) => elemento.id == id);
+     // console.log(producto);
+//     if (!producto){
+//         return res.status(404).json({ error: "No se encontro el producto"});
+//     }
 
-    producto.nombre = nombre;
+//     producto.nombre = nombre;
 
-    res.json(producto)
-});
+//     res.json(producto)
+// });
 
-router.delete("/:id",(req, res) => {
-    const{ id } = req.params;
+// router.delete("/:id",(req, res) => {
+//     const{ id } = req.params;
 
-    const producto = productos.find((elemento) => elemento.id == id);
+//     const producto = productos.find((elemento) => elemento.id == id);
 
-    if (!producto){
-        return res.status(404).json({ error: "No se encontro el producto"});
-    }
+//     if (!producto){
+//         return res.status(404).json({ error: "No se encontro el producto"});
+//     }
 
-    const productoIndex = productos.findIndex((elemento) => elemento.id == id);
-    productos.splice(productoIndex, 1);
+//     const productoIndex = productos.findIndex((elemento) => elemento.id == id);
+//     productos.splice(productoIndex, 1);
     
-    res.send(producto);
+//     res.send(producto);
+
+// })
 
 
-})
+router.put("/:id", controller.update);
+router.delete("/:id", controller.destroy);
 
 
 module.exports = router;
